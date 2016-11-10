c06e3c5e4160d68644715e54517dc164f8889a60
evarmentaa <evelin.araujo.536@my.csun.edu>
2016-11-09 18:52:12 -0800

    update, everything is now up and running

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -1,44 +1,163 @@
 # Add your VPC ID to default below
 variable "vpc_id" {
   description = "VPC ID for usage throughout the build process"
-  default = ""
+  default = "vpc-429b6c25"
 }
 
 provider "aws" {
   region = "us-west-2"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
 }
 
+###### Creates Single Internet Gateway ######
 resource "aws_internet_gateway" "gw" {
-  vpc_id = "${var.vpc_id}"
+  vpc_id = "vpc-429b6c25"
 
   tags = {
     Name = "default_ig"
   }
 }
 
+################ Create NAT gateway ##########
+resource "aws_nat_gateway" "nat" {
+  allocation_id = "${aws_eip.lb.id}"
+  subnet_id = "${aws_subnet.private_subnet_a.id}"
+ } 
+resource "aws_eip" "lb" {
+  depends_on = ["aws_internet_gateway.gw"]
+  vpc = true
+}
+
+##### Creates public route table ######
 resource "aws_route_table" "public_routing_table" {
+    vpc_id ="${var.vpc_id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.gw.id}"
+    }
+
+    tags {
+        Name = "public_routing_table"
+    }
+}
+
+########## Creates private route table ##########
+resource "aws_route_table" "private_routing_table" {
   vpc_id = "${var.vpc_id}"
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.gw.id}"
+    nat_gateway_id = "${aws_nat_gateway.nat.id}"
   }
-
   tags {
-    Name = "public_routing_table"
+    Name = "private_routing_table"
   }
 }
 
+
+####### Creates 3 public Subnets ############ 
 resource "aws_subnet" "public_subnet_a" {
     vpc_id = "${var.vpc_id}"
-    cidr_block = ""
+    #giving it 256 addresses
+    cidr_block = "172.32.1.0/24"
     availability_zone = "us-west-2a"
 
     tags {
-        Name = "public_a"
+        Name = "public_subneta"
     }
 }
+
+resource "aws_subnet" "public_subnet_b" {
+    vpc_id = "${var.vpc_id}"
+    cidr_block = "172.32.2.0/24"
+    availability_zone = "us-west-2b"
+
+    tags {
+        Name = "public_subnetb"
+    }
+}
+
+resource "aws_subnet" "public_subnet_c" {
+    vpc_id = "${var.vpc_id}"
+    cidr_block = "172.32.3.0/24"
+    availability_zone = "us-west-2c"
+
+    tags {
+        Name = "public_subnetc"
+    }
+}
+
+########## Creates 3 private subnets ############
+resource "aws_subnet" "private_subnet_a" {
+    vpc_id = "${var.vpc_id}"
+    cidr_block = "172.32.16.0/22"
+    availability_zone = "us-west-2a"
+
+    tags {
+        Name = "private_a"
+    }
+}
+
+resource "aws_subnet" "private_subnet_b" {
+    vpc_id = "${var.vpc_id}"
+    cidr_block = "172.32.20.0/22"
+    availability_zone = "us-west-2b"
+
+    tags {
+        Name = "private_b"
+    }
+}
+
+resource "aws_subnet" "private_subnet_c" {
+    vpc_id = "${var.vpc_id}"
+    cidr_block = "172.32.24.0/22"
+    availability_zone = "us-west-2c"
+
+    tags {
+        Name = "private_c"
+    }
+}
+###### Route table Association ###########
 
 resource "aws_route_table_association" "public_subnet_a_rt_assoc" {
     subnet_id = "${aws_subnet.public_subnet_a.id}"
     route_table_id = "${aws_route_table.public_routing_table.id}"
+}
+
+resource "aws_route_table_association" "public_subnet_b_rt_assoc" {
+    subnet_id = "${aws_subnet.public_subnet_b.id}"
+    route_table_id = "${aws_route_table.public_routing_table.id}"
+}
+
+resource "aws_route_table_association" "public_subnet_c_rt_assoc" {
+    subnet_id = "${aws_subnet.public_subnet_c.id}"
+    route_table_id = "${aws_route_table.public_routing_table.id}"
+}
+
+
+######### Bastion Instance #########
+resource "aws_instance" "bastion" {
+    ami = "ami-5ec1673e"
+    associate_public_ip_address = true
+    subnet_id = "${aws_subnet.public_subnet_a.id}"
+    instance_type = "t2.micro"
+    key_name = "${var.aws_key_name}"
+    security_groups = ["${aws_security_group.bastion.id}"]
+    tags {
+        Name = "Bastion"
+    }
+}
+
+################### Security Group for Bastion instance
+resource "aws_security_group" "bastion" {
+	name = "bastion"
+	description = "Allow access from your current public IP address to an instance on port 22 (SSH)"
+	ingress {
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["172.32.0.0/16"]
+	}
+
+	vpc_id = "${var.vpc_id}"
 }
